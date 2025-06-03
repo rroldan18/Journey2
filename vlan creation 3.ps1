bde07c1c4222e9852a711603b45b7529ba669487
rroldan18 <rodenroldan18@gmail.com>
2025-06-03 15:31:19 +0800

    Create vlan creation 3.ps1

---
+++ vlan creation 3.ps1
@@ -0,0 +1,24 @@
+﻿D1: vlan creation + dhcp + svi:
+config t 
+vlan 101
+ name vlan101.com.ph
+ exit
+Int vlan 101
+ no shut
+ ip add 10.0.4.1 255.255.252.0
+ ip ospf 1 area 0
+ip dhcp excluded-add 10.0.4.1 10.0.4.100
+ip dhcp pool vlan101.com.ph   
+ network 10.0.4.0 255.255.252.0
+ default-router 10.0.4.0
+ domain-name vlan101.com.ph
+ @A2:
+ conf t
+ Int e 1/0
+  switchport mode access
+  switchport access vlan 101
+  @S1:
+  conf t
+  Int e 1/0
+  no shut
+  ip address dhcp
